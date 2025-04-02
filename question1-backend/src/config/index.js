256b78d5f2353ab6b9d98a5db00b6b5863461cb9
Manish Kumar <139037481+kmanishprogrammar@users.noreply.github.com>
2025-04-02 21:16:06 +0530

    updated

--- question1-backend/src/config/index.js
+++ question1-backend/src/config/index.js
@@ -1,13 +1,12 @@
-// src/config/index.js
 require('dotenv').config();
 
 module.exports = {
     testServerBaseUrl: process.env.TEST_SERVER_BASE_URL,
     userEmail: process.env.USER_EMAIL,
-    userName: process.env.USER_NAME,     // Make sure this line is present
+    userName: process.env.USER_NAME,     
     rollNo: process.env.ROLL_NO,
     accessCode: process.env.ACCESS_CODE,
-    clientId: process.env.CLIENT_ID,     // Ensure this key ('clientId') is consistent
-    clientSecret: process.env.CLIENT_SECRET, // Ensure this key ('clientSecret') is consistent
+    clientId: process.env.CLIENT_ID,     
+    clientSecret: process.env.CLIENT_SECRET,
     port: process.env.PORT || 3001,
 };
